32c6f394e5ddf33a16a65212c71a76dda2f619ae
Richelmy Lopes <richelmylopes@hotmail.com>
2019-08-25 23:17:14 -0300

    ajustes

--- Projeto - Techno/app.js
+++ Projeto - Techno/app.js
@@ -4,49 +4,65 @@ const vm = new Vue({
         produtos: {},
         produto: {},
         carrinho: [],
+        mensagemAlerta: "Item Adicionado",
+        alertaAtivo: false,
     },
     methods: {
-        fetchProdutos(){
+        fetchProdutos() {
             fetch("./api/produtos.json")
-            .then(r => r.json())
-            .then((body) =>{
-                this.produtos = body;
-            });
+                .then(r => r.json())
+                .then((body) => {
+                    this.produtos = body;
+                });
         },
-        fetchProduto(id){
+        fetchProduto(id) {
             fetch(`./api/produtos/${id}/dados.json`)
-            .then(r => r.json())
-            .then(r =>{
-                this.produto = r;   
-            });
+                .then(r => r.json())
+                .then(r => {
+                    this.produto = r;
+                });
         },
-        fecharModal({target, currentTarget}) {
-            if(target === currentTarget){
-            this.produto = false;
+        fecharModal({ target, currentTarget }) {
+            if (target === currentTarget) {
+                this.produto = false;
             }
         },
-        abrirModal(id){
+        abrirModal(id) {
             this.fetchProduto(id);
             window.scrollTo({
-               top: 0,
-               behavior: "smooth",
+                top: 0,
+                behavior: "smooth",
             });
         },
-        adicionarItem(){
+        adicionarItem() {
             this.produto.estoque--;
-            const {id, nome, preco} = this.produto;
-            this.carrinho.push({id, nome, preco});
+            const { id, nome, preco } = this.produto;
+            this.carrinho.push({ id, nome, preco });
+            this.alerta(`${nome} adicionado ao carrinho`)
         },
-        removerItem(index){
+        removerItem(index) {
             this.produto.estoque++;
             this.carrinho.splice(index, 1);
+        },
+
+        checarLocalStorage() {
+            if (window.localStorage.carrinho) {
+                this.carrinho = JSON.parse(window.localStorage.carrinho);
+            }
+        },
+        alerta(mensagem) {
+            this.mensagemAlerta = mensagem;
+            this.alertaAtivo = true;
+            setTimeout(() => {
+                this.alertaAtivo = false;
+            }, 1500);
         }
     },
     computed: {
-        carrinhoTotal(){
+        carrinhoTotal() {
             let total = 0;
-            if(this.carrinho.length){
-                this.carrinho.forEach(item =>{
+            if (this.carrinho.length) {
+                this.carrinho.forEach(item => {
                     total += item.preco;
                 });
             }
@@ -56,16 +72,24 @@ const vm = new Vue({
     },
 
     filters: {
-        numeroPreco(valor){
-            return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
+        numeroPreco(valor) {
+            return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
         },
-        maiusculo(valor){
-           return valor.toUpperCase();
+        maiusculo(valor) {
+            return valor.toUpperCase();
         }
     },
 
-    created(){
+    watch: {
+        carrinho() {
+            window.localStorage.carrinho = JSON.stringify(this.carrinho);
+        }
+
+    },
+    created() {
         this.fetchProdutos();
+        this.checarLocalStorage();
+        this.fetchProduto("notebook");
     }
 
 });
